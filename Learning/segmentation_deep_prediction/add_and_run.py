b00119bb4a5f0c28b57906b25a417cef38b4f6d9
Falightbe <wangmenglan24@gmail.com>
2017-07-03 12:32:57 +0200

    Test parallel

--- Learning/segmentation_deep_prediction/add_and_run.py
+++ Learning/segmentation_deep_prediction/add_and_run.py
@@ -807,16 +807,16 @@ def main(argv):
 
 					# Predict subwindow masks
 					print("Prediction of %d subwindows for tile %d " % (n_subw, wsi))
-					# _Y = predict(_X, prediction_model, mean = training_sample_mean, std = training_sample_std)
-					n_jobs, _, starts = _partition_images(parameters['nb_jobs'], n_subw)
-					print(n_jobs)
-					print(starts)
-					_Y = Parallel(n_jobs = n_jobs)(
-						delayed(predict)(_X[starts[i]:starts[i+1]], prediction_model, mean = training_sample_mean, std = training_sample_std) for i in xrange(n_jobs))
+					_Y = predict(_X, prediction_model, mean = training_sample_mean, std = training_sample_std)
+					# n_jobs, _, starts = _partition_images(parameters['nb_jobs'], n_subw)
+					# print(n_jobs)
+					# print(starts)
+					# _Y = Parallel(n_jobs = n_jobs)(
+					# 	delayed(predict)(_X[starts[i]:starts[i+1]], prediction_model, mean = training_sample_mean, std = training_sample_std) for i in xrange(n_jobs))
 
 
 					_Y = np.reshape(_Y, (n_subw, parameters['pyxit_target_width'], parameters['pyxit_target_height']))
-					quit()
+					# quit()
 					# Build tile mask from subwindow predictions
 					tile_mask = np.zeros((height, width), dtype = np.float)
 					# it = 0
