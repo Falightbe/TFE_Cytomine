bfaa748102f05c20593d96cf797cfa7d909f987b
Falightbe <wangmenglan24@gmail.com>
2017-07-17 11:32:37 +0200

    Try gpu

--- Learning/segmentation_deep_prediction/add_and_run.py
+++ Learning/segmentation_deep_prediction/add_and_run.py
@@ -222,40 +222,40 @@ def dice_coef_loss(y_true, y_pred):
 
 def get_unet(imgs_width, imgs_height):
 	inputs = Input((imgs_width, imgs_height, 3))
-	conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
-	conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+	conv1 = Conv2D(8, (3, 3), activation='relu', padding='same')(inputs)
+	conv1 = Conv2D(8, (3, 3), activation='relu', padding='same')(conv1)
 	pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-	conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
-	conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+	conv2 = Conv2D(16, (3, 3), activation='relu', padding='same')(pool1)
+	conv2 = Conv2D(16, (3, 3), activation='relu', padding='same')(conv2)
 	pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-	conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
-	conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+	conv3 = Conv2D(32, (3, 3), activation='relu', padding='same')(pool2)
+	conv3 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv3)
 	pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-	conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
-	conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+	conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool3)
+	conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv4)
 	pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-	conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
-	conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+	conv5 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool4)
+	conv5 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv5)
 
 	up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=3)
-	conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
-	conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+	conv6 = Conv2D(64, (3, 3), activation='relu', padding='same')(up6)
+	conv6 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv6)
 
 	up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3], axis=3)
-	conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
-	conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+	conv7 = Conv2D(32, (3, 3), activation='relu', padding='same')(up7)
+	conv7 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv7)
 
 	up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2], axis=3)
-	conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
-	conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+	conv8 = Conv2D(16, (3, 3), activation='relu', padding='same')(up8)
+	conv8 = Conv2D(16, (3, 3), activation='relu', padding='same')(conv8)
 
 	up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1], axis=3)
-	conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
-	conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+	conv9 = Conv2D(8, (3, 3), activation='relu', padding='same')(up9)
+	conv9 = Conv2D(8, (3, 3), activation='relu', padding='same')(conv9)
 
 	conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
 
