07edb85efa9d8c6e15e823b1afeacc659205e0d8
Falightbe <wangmenglan24@gmail.com>
2017-07-29 10:43:36 +0200

    Conv2DTransposed

--- Learning/segmentation_deep_prediction/add_and_run.py
+++ Learning/segmentation_deep_prediction/add_and_run.py
@@ -224,19 +224,19 @@ def get_unet(imgs_width, imgs_height):
 	inputs = Input((imgs_width, imgs_height, n_channels))
 	conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
 	conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
-	pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) 64
+	pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
 	conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
 	conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
-	pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) 32
+	pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
 	conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
 	conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
-	pool3 = MaxPooling2D(pool_size=(2, 2))(conv3) 16
+	pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
 	conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
 	conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
-	pool4 = MaxPooling2D(pool_size=(2, 2))(conv4) 8
+	pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
 	conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
 	conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
